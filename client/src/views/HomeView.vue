17d8d51f05b9660c06893c972cad40a5d5c373ec
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-08 00:14:04 -0300

    feat: Edit homeview with homeview app template

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+    import HomeHeader from '@/components/HomeHeader.vue';
+    import MovieCard from '@/components/MovieCard.vue';
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <HomeHeader />
+    <MovieCard class="card" link="https://via.placeholder.com/150" moviename="Sample Movie" />
 </template>
+
+<style>
+    .card {
+        margin: 2rem auto;
+    }
+</style>
