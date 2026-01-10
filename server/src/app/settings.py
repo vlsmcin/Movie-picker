ef1a451932c28efaec7fdcc5f87a4d5d65ea4bbc
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-10 13:02:03 -0300

    feat: Create users_movies app for relationship database

--- server/src/app/settings.py
+++ server/src/app/settings.py
@@ -34,6 +34,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'movies.apps.MoviesConfig',
     'users.apps.UsersConfig',
+    'users_movies.apps.UsersMoviesConfig',
     'polls.apps.PollsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
---
+++ server/src/users_movies/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class UsersMoviesConfig(AppConfig):
+    name = 'users_movies'
