0aeb749c75c8103333c90db11bab17df7e0db972
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-12 11:47:28 -0300

    refactor: Remove genre specific and relationship folders

--- server/src/movies_genres/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class MoviesGenresConfig(AppConfig):
-    name = 'movies_genres'
--- server/src/users_movies/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class UsersMoviesConfig(AppConfig):
-    name = 'users_movies'
