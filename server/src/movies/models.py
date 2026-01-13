76cba2d66adaab1844e119756790510c9fff4497
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-13 10:06:13 -0300

    fix: Change id to tmdb_id to reference the API

--- server/src/movies/models.py
+++ server/src/movies/models.py
@@ -6,7 +6,7 @@ API_URL = "https://api.themoviedb.org/3"
 
 # Create your models here.
 class Movie(models.Model):
-    id = models.AutoField(primary_key=True)
+    tmdb_id = models.IntegerField(unique=True)
     title = models.CharField(max_length=200)
     overview = models.TextField()
     vote_average = models.FloatField()
@@ -20,7 +20,7 @@ class Movie(models.Model):
         return self.title
     
 class Genre(models.Model):
-    id = models.AutoField(primary_key=True)
+    tmdb_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=100)
 
     def __str__(self):
