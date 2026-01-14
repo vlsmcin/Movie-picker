2a29302d681d96b9a3e7e4303fb8961076c794e6
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-14 02:40:09 -0300

    fix: Fix url declaration in main app

--- server/src/movies/urls.py
+++ server/src/movies/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("movies/<str:movie_title>", views.get_movie_by_title, name="movie_list"),
+    path("<str:movie_title>/", views.get_movie_by_title, name="get_movie_by_title"),
 ]
