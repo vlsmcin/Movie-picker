7e087554dc33e3f723c02dc4001ec2fd361e7380
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-18 10:27:17 -0300

    tests: Create tests for user creation

--- server/src/users/tests.py
+++ server/src/users/tests.py
@@ -1,3 +1,47 @@
 from django.test import TestCase
 
 # Create your tests here.
+class UserTests(TestCase):
+    def test_create_user(self):
+        """
+        Test to ensure that a user can be created successfully.
+        """
+        response = self.client.post('/api/v1/users/', {
+            'username': 'testuser',
+            'email': 'testuser@example.com',
+            'password': 'securepassword123'
+        })
+        self.assertEqual(response.status_code, 201)
+        self.assertIn('id', response.data)
+        self.assertEqual(response.data['username'], 'testuser')
+        self.assertEqual(response.data['email'], 'testuser@example.com')
+
+    def test_create_user_missing_email(self):
+        """
+        Test to ensure that user creation fails when required fields are missing.
+        """
+        response = self.client.post('/api/v1/users/', {
+            'username': 'testuser',
+            'password': 'securepassword123'
+        })
+
+        self.assertEqual(response.status_code, 400)
+
+    def test_create_user_missing_password(self):
+        """
+        Test to ensure that user creation fails when password is missing.
+        """
+        response = self.client.post('/api/v1/users/', {
+            'username': 'testuser',
+            'email': 'testuser@example.com'
+        })
+
+        self.assertEqual(response.status_code, 400)
+
+# class UserMoviesTests(TestCase):
+#     def setUp(self):
+#         """
+#         Set up a user and some movies for testing.
+#         """
+#         self.user = User.objects.create_user(
+#             username='movielover',
