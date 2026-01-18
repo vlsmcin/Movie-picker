6287f9cf472f9c56823189ac65d7810a30ff0125
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-18 10:35:43 -0300

    feat: Add url in router to UserMovies views

--- server/src/users/urls.py
+++ server/src/users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import UserCreateView
+from .views import UserCreateView, UserMoviesView
 
 urlpatterns = [
     path("", UserCreateView.as_view(), name="user-create"),
+    path("movies/<int:user_id>/", UserMoviesView.as_view(), name="user-movies"),
 ]
