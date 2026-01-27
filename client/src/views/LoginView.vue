2af09813c035137dc1e4ebcb9942d5ec7d5548d1
vlsmcin <viniciuslimamelo7@gmail.com>
2026-01-27 01:00:15 -0300

    feat: Add error tratative on login view

--- client/src/views/LoginView.vue
+++ client/src/views/LoginView.vue
@@ -1,26 +1,112 @@
 <script lang="ts" setup>
     import { ref } from 'vue';
     import { useUserAuthStore } from '@/stores/user';
+    import router from '@/router';
+    import Toast from '@/components/Toast.vue';
 
     const userAuthStore = useUserAuthStore();
-    const username = ref('');
     const email = ref('');
     const password = ref('');
+    const emptyEmail = ref(false);
+    const emptyPassword = ref(false);
+    const showToast = ref(false);
+    const toastMessage = ref('');
+    const type = ref<'success' | 'error'>('error');
 
     const login = async () => {
         try {
-            await userAuthStore.login(username.value, email.value, password.value);
+            await userAuthStore.login(email.value, password.value);
+            showToast.value = true;
+            toastMessage.value = 'Login bem-sucedido!';
+            type.value = 'success';
             // Redirect or show success message
         } catch (error) {
             // Handle login error
             console.error(error);
+            showToast.value = true;
+            type.value = 'error';
+            toastMessage.value = 'Login falhou. Por favor, verifique suas credenciais e tente novamente.';
         }
     };
+
+    const handleLogin = () => {
+        if (!email.value) {
+            emptyEmail.value = true;
+        } else {
+            emptyEmail.value = false;
+        }
+
+        if (!password.value) {
+            emptyPassword.value = true;
+        } else {
+            emptyPassword.value = false;
+        }
+
+        if (emptyEmail.value || emptyPassword.value) {
+            showToast.value = true;
+            type.value = 'error';
+            toastMessage.value = 'Por favor, preencha todos os campos obrigatórios.';
+            return;
+        }
+        
+        login();
+    }
 </script>
 
 <template>
-    <input type="text" placeholder="Username" v-model="username" />
-    <input type="email" placeholder="Email" v-model="email" />
-    <input type="password" placeholder="Password" v-model="password" />
-    <button @click="login">Login</button>
+    <Toast v-if="showToast" :message="toastMessage" @close="showToast = false" :type="type"/>
+    <div class="login-view">
+        <h1 class="title">Login</h1>
+        <input :class="{'empty-email': emptyEmail}" type="text" placeholder="Email" v-model="email" />
+        <input :class="{'empty-password': emptyPassword}" type="password" placeholder="Senha" v-model="password" />
+        <button class="login-options" @click="handleLogin">Entrar</button>
+        <button class="login-options" @click="() => router.push('/register')">Cadastrar-se</button>
+    </div>
 </template>
+
+<style scoped>
+    .login-view {
+        display: flex;
+        flex-direction: column;
+        max-width: 400px;
+        margin: 2rem auto;
+        padding: 2rem;
+        border: 1px solid var(--color-border);
+        border-radius: 10px;
+        box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+        position: relative;     
+    }
+
+    input {
+        display: block;
+        margin: 0.4rem 0;
+        padding: 0.5rem;
+        font-size: 1rem;
+    }
+
+    .login-options {
+        padding: 0.5rem 1rem;
+        margin: 0.25rem 0;
+        font-size: 1rem;
+        background-color: var(--color-primary);
+        color: white;
+        border: solid 1px #474745;
+        border-radius: 5px;
+        cursor: pointer;
+    }
+
+    .login-options:hover {
+        background-color: #474745;
+    }
+
+    .empty-email, .empty-password {
+        border: 1px solid red;
+        border-radius: 5px;
+    }
+
+    .title {
+        text-align: center;
+        margin-bottom: 1rem;
+    }
+
+</style>
