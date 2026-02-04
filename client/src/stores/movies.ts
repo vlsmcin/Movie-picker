c42f1a19a9d2e06e4e16fe65cbf8f9fb7f6b9e7a
vlsmcin <viniciuslimamelo7@gmail.com>
2026-02-04 15:57:41 -0300

    fix: Change store logic to only store the movies from user, not all movies

--- client/src/stores/movies.ts
+++ client/src/stores/movies.ts
@@ -1,28 +1,29 @@
 import { defineStore } from "pinia";
 import movieService from "@/services/movieService";
-import type { Movie } from "@/types/movie";
+import type { Movie, UserMovie } from "@/types/movie";
 
-export const useMoviesStore = defineStore("movies", {
+export const useUserMoviesStore = defineStore("movies", {
   state: () => ({
-    movies: [] as Array<Movie>,
+    userMovies: [] as Array<UserMovie>,
+    isLoaded: false,
+    isLoading: false,
   }),
 
-  getters: {
-    getAllMovies: (state) => state.movies,
-    getMoviesByGenre: (state) => (genre: string) =>
-      state.movies.filter((movie) => movie.genres.includes(genre)),
-  },
-
   actions: {
-    async addMovie(movie: Movie, watched: boolean, in_watchlist: boolean): Promise<any> {
-      const response = await movieService.addMovieForUser(movie, watched, in_watchlist).then((addedMovie) => {
-        this.movies.push(addedMovie);
+    async addMovie(userMovie: UserMovie) {
+      await movieService.addMovieForUser(userMovie).then((addedMovie) => {
+        this.userMovies.push(addedMovie);
       });
-      return response;
     },
-
-    async fetchMoviesByTitle(title: string) {
-      this.movies = await movieService.getMoviesByTitle(title);
+    async fetchMoviesIfEmpty(force = false) {
+      if (this.userMovies.length === 0 || force) {
+        try {
+          const movies = await movieService.getMoviesForUser();
+          this.userMovies = movies;
+        } catch (error) {
+          console.error("Failed to fetch movies:", error);
+        }
+      }
     }
   },
 });
